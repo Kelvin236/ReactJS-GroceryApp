45b0b50626e224371557443898c37f866f983ab4
Kelvin Lawson <Kelvin236@users.noreply.github.com>
2019-03-20 11:54:04 -0300

    Adding the scrolling feature
    I am implementing a feature that allows the user to click on the links in the navigation bar. Once a link is clicked the page scrolls, at a reasonable speed, to the particular section of the UI that is connected to that link. 
    
    There are 3 parts of the header.js file that I want to learn more about. 1) the constructor function, 2) the componentDidMount code section and 3) the componentDidUnmount code piece. Specifically, I want to know more about exactly what they do.
    
    Furthermore, there are a few things that I will change:
        1) This is not a huge project so I do not need several component files. I just need one index page containing all the code.
        2) I will remove some of the imports from the react-scroll package because I do not need them. I just need to figure out what 
            is truly relevant/irrelevant for this project.

--- src/components/header_component/header.js
+++ src/components/header_component/header.js
@@ -1,29 +1,89 @@
 import React, { Component } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+
 
 class Header extends Component {
-  render() {
-    return (
-        <header>
-            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Website Name</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#aboutus">About Us</Nav.Link>
-                        <Nav.Link href="#services">Services</Nav.Link>
-                        <Nav.Link href="#contact">Contact</Nav.Link>
-                    </Nav>
+    constructor(props){
+        super(props);
+        this.scrollToTop = this.scrollToTop.bind(this);
+    }
 
-                    <Nav>
-                        <Nav.Link href="#login">Login</Nav.Link>
-                        <Nav.Link href="#signup">Sign Up</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        </header>
+    componentDidMount() {
+
+        Events.scrollEvent.register('begin', function () {
+            console.log("begin", arguments);
+        });
+
+        Events.scrollEvent.register('end', function () {
+            console.log("end", arguments);
+        });
+    }
+
+    scrollToTop() {
+        scroll.scrollToTop();
+    }
+
+    scrollTo() {
+        scroller.scrollTo('scroll-to-element', {
+            duration: 800,
+            delay: 0,
+            smooth: 'easeInOutQuart'
+        })
+    }
+
+    scrollToWithContainer() {
+        let goToContainer = new Promise ((resolve, reject) => {
+
+            Events.scrollEvents.register('end', () => {
+                resolve();
+                Events.scrollEvents.remove('end');
+            });
+
+            scroller.ScrollTo('scroll-container', {
+                duration: 800,
+                delay: 0,
+                smooth: 'easeInOutQuart'
+            });
+        });
+
+        goToContainer.then(() => 
+          scroller.scrollTo('scroll-container-second-element', {
+              duration: 800,
+              delay: 0,
+              smooth: 'easeInOutQuart',
+              containerId: 'scroll-container'
+          }));
+    }
+
+    componentWillUnmount() {
+        Events.scrollEvent.remove('begin');
+        Events.scrollEvent.remove('end');
+    }
+
+
+   render() {
+     return (
+         <header>
+             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                 <Navbar.Brand href="#home">Website Name</Navbar.Brand>
+                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                 <Navbar.Collapse id="responsive-navbar-nav">
+                     <Nav className="mr-auto">
+                         <Nav.Link><Link activeClass="active" className="aboutus" to="aboutus" spy={true} smooth={true} duration={500}>About Us</Link></Nav.Link>
+                         <Nav.Link><Link activeClass="active" className="services" to="services" spy={true} smooth={true} duration={500}>Services</Link></Nav.Link>
+                         <Nav.Link href="#contact">Contact</Nav.Link>
+                     </Nav>
+
+                     <Nav>
+                         <Nav.Link href="#login">Login</Nav.Link>
+                         <Nav.Link href="#signup">Sign Up</Nav.Link>
+                     </Nav>
+                 </Navbar.Collapse>
+             </Navbar>
+         </header>
     );
-  }
+   }
 }
 
 export default Header;
